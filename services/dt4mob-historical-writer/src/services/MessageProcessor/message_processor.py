8739055348f689cf3e3b8afaacdfec888fab8ed8
fungame2270 <tomasvictal@gmail.com>
2026-06-26 14:45:18 +0100

    added override feature based on present header in eclipse ditto

--- services/dt4mob-historical-writer/src/services/MessageProcessor/message_processor.py
+++ services/dt4mob-historical-writer/src/services/MessageProcessor/message_processor.py
@@ -1,11 +1,15 @@
 from src.models.ditto_events import DittoEvent,Action
 
 
-def parse_message(msg):
+def parse_message(msg) -> DittoEvent:
     topic_processed = msg["topic"].split("/")
 
+    headers = msg.get("headers", {})
+    override_timestamp = headers.get("dt4mob-historic-timestamp-override")
+    event_time = override_timestamp if override_timestamp else msg["timestamp"]
+
     dittomsg = DittoEvent(
-        time=msg["timestamp"],
+        time=event_time,
         thing_id=f"{topic_processed[0]}:{topic_processed[1]}",
         action=Action(topic_processed[5]),
         path=msg["path"],
